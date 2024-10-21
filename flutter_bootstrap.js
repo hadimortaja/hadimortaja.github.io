4230717d281805a3d9610b1a783d53dc6c9a402c
hadimortaja <hadimortaja9@gmail.com>
2024-10-22 02:51:10 +0300

    Deploy Flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1062225558"
+    serviceWorkerVersion: "4173227102"
   }
 });
